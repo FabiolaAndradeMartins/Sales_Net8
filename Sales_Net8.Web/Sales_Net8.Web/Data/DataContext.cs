3502878c6817394986e215746d2ffacf1199caa2
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-09-03 19:15:45 +0100

    Adicionando a tabela Category no Contexto e na Base de Dados

--- Sales_Net8.Web/Sales_Net8.Web/Data/DataContext.cs
+++ Sales_Net8.Web/Sales_Net8.Web/Data/DataContext.cs
@@ -11,11 +11,13 @@ namespace Sales_Net8.Web.Data
         }
 
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
 
     }
